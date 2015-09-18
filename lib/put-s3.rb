d93b39bb54d110a5f5e68808ba1a411995b0fbca
kappa <kappa@kappabookair.local>
2015-09-18 15:35:59 +0900

    S3 URL をドメイン名で返すように修正

--- lib/put-s3.rb
+++ lib/put-s3.rb
@@ -11,12 +11,12 @@ def put_graph_s3(file_name, date)
   key = File.basename(file_name)
   obj = s3.bucket(ENV['S3_BUCKET']).object("#{date}/#{key}")
   obj.upload_file(file_name)
-  obj.public_url
+  obj.public_url(virtual_host: true)
 end
 
 def put_html_s3(file_name)
   key = File.basename(file_name)
   obj = s3.bucket(ENV['S3_BUCKET']).object("kyusyu/#{key}")
   obj.upload_file(file_name)
-  obj.public_url
+  obj.public_url(virtual_host: true)
 end
