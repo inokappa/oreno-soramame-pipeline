5ac22f597cb40e1fe3ec6d8cdbce410cf612af09
kappa <kappa@kappabookair.local>
2015-09-18 22:49:59 +0900

    ajust graph width.

--- lib/plot-graph.rb
+++ lib/plot-graph.rb
@@ -5,7 +5,7 @@ require 'gchart'
 def plot_graph(check_points, points, title, check_station, check_station_code, date)
   check_points = check_points.join("|")
   chart = Gchart.new( 
-    :size => '325x200',
+    :size => '350x200',
     :type => 'line',
     :title => "#{check_station} - #{title}" ,
     :theme => :greyscale,
